91781dfa992b7655fae1f80d8b248c55c320e40f
zhangxinfang520 <44743757+zhangxinfang520@users.noreply.github.com>
2021-09-06 20:26:29 +0800

    Update 401_CNN.py

--- tutorial-contents/401_CNN.py
+++ tutorial-contents/401_CNN.py
@@ -52,7 +52,8 @@ train_loader = Data.DataLoader(dataset=train_data, batch_size=BATCH_SIZE, shuffl
 
 # pick 2000 samples to speed up testing
 test_data = torchvision.datasets.MNIST(root='./mnist/', train=False)
-test_x = torch.unsqueeze(test_data.test_data, dim=1).type(torch.FloatTensor)[:2000]/255.   # shape from (2000, 28, 28) to (2000, 1, 28, 28), value in range(0,1)
+# shape from (2000, 28, 28) to (2000, 1, 28, 28), value in range(0,1)
+test_x = torch.unsqueeze(test_data.test_data, dim=1).type(torch.FloatTensor)[:2000]/255.   
 test_y = test_data.test_labels[:2000]
 
 
